fa667ca0eafa68c494a6a6d39939c4d9720249d7
jonatanesteve <jonatanesteve3@gmail.com>
2022-08-07 21:00:36 -0300

    agregado de JS para animacion de Typewriter, correcion de responsive, agregado de detalles

---
+++ script.js
@@ -0,0 +1,7 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+    strings : ['Bienvenidos a Krazo Propiedades', 'Tenemos la vivienda indicada para vos'],
+    autoStart: true,
+    loop: true,
+});
